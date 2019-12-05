498353bd70997cc787a5a92845a326aa5466ec7b
Alberto Hernandez <alberto@albertoX1>
2019-12-05 08:45:23 +0100

    filter autor #13

--- searchapp/src/main/ng5/src/app/components/article-result/article-result.component.ts
+++ searchapp/src/main/ng5/src/app/components/article-result/article-result.component.ts
@@ -157,7 +157,7 @@ export class ArticleResultComponent implements OnInit {
         //    this.onSearchAuthor.emit(s);
         let c = new Criterium();
         c.field = 'autor';
-        c.value = '"' + s + '"';
+        c.value = s;
         //this.router.navigate(['/hledat/cokoliv', {criteria: JSON.stringify([c]), start: 0}])
         this.router.navigate(['/'+this.state.ctx.ctx+'/hledat/cokoliv', {criteria: JSON.stringify([c]), start: 0}],
         {queryParamsHandling: "preserve"})
