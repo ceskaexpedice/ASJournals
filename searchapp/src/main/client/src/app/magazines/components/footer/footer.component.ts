8461fcacbee8693169db27a444943ba917c5649c
Petr Kudela <kudy.samba@gmail.com>
2023-11-08 15:25:24 +0100

    footer - flex layout replacement, style has been deleted and code has been moved to the component style

--- searchapp/src/main/client/src/app/magazines/components/footer/footer.component.ts
+++ searchapp/src/main/client/src/app/magazines/components/footer/footer.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   standalone: true,
-  imports: [FlexLayoutModule],
+  imports: [],
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
