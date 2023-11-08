6ba114edcbdee18810af67674d5a9bc8fd56228a
Petr Kudela <kudy.samba@gmail.com>
2023-11-08 15:16:21 +0100

    navbar - flex layout replacement, layout modification, unnecesary styles has been deleted

--- searchapp/src/main/client/src/app/magazines/components/navbar/navbar.component.ts
+++ searchapp/src/main/client/src/app/magazines/components/navbar/navbar.component.ts
@@ -6,12 +6,11 @@ import { MagazinesService } from '../../magazines.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, TranslateModule, MatToolbarModule, MatIconModule, FlexLayoutModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, TranslateModule, MatToolbarModule, MatIconModule, MatButtonModule],
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
