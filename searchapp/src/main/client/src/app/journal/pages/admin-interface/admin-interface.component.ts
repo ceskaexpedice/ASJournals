b4b2adb818a7ba0f94bf3dba68450906c1f85270
Petr Kudela <kudy.samba@gmail.com>
2023-11-22 20:57:12 +0100

    admin / rozhrani - the layou has been setted

--- searchapp/src/main/client/src/app/journal/pages/admin-interface/admin-interface.component.ts
+++ searchapp/src/main/client/src/app/journal/pages/admin-interface/admin-interface.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularSplitModule } from 'angular-split';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-admin-interface',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AngularSplitModule, MatCheckboxModule, TranslateModule, MatDividerModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './admin-interface.component.html',
   styleUrls: ['./admin-interface.component.scss']
 })
