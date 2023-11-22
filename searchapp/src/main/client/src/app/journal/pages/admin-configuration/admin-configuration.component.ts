5b713534014e1d5d7f04c977b2a12d5541309255
Petr Kudela <kudy.samba@gmail.com>
2023-11-22 16:15:31 +0100

    admin / kofngirutrace - new layout has been setted

--- searchapp/src/main/client/src/app/journal/pages/admin-configuration/admin-configuration.component.ts
+++ searchapp/src/main/client/src/app/journal/pages/admin-configuration/admin-configuration.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-admin-configuration',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatButtonModule, TranslateModule, MatDividerModule, MatRadioModule],
   templateUrl: './admin-configuration.component.html',
   styleUrls: ['./admin-configuration.component.scss']
 })
