070d8615312f54a842422a33c2eabce88f6a58eb
Petr Kudela <kudy.samba@gmail.com>
2023-11-24 17:56:05 +0100

    article viewer / detail - the layout has been setted

--- searchapp/src/main/client/src/app/journal/pages/article-viewer-details/article-viewer-details.component.ts
+++ searchapp/src/main/client/src/app/journal/pages/article-viewer-details/article-viewer-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-article-viewer-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './article-viewer-details.component.html',
   styleUrls: ['./article-viewer-details.component.scss']
 })
